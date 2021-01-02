66cf440bc92137bf92ce15fd020004d194067ae2
Abdoul Hanane <gbadhanane@gmail.com>
2021-01-02 07:48:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+choice = input('You want to go "left"or "right". Type "l" for left and "r" fo right\n')
+if(choice == 'r' or choice == 'R'):
+   print("You felt into a hole, Game Over")
+else:
+  print("You are at the station, which transportation method did you want to use\n")
+  choice = input(" Enter c for car, a for plane and b for boat\n")
+  if choice == 'c'or choice == 'C':
+    print("A rhino hit your car, Game over")
+  elif choice == 'a' or choice =='A':
+    print("Thunder strike your plane, Game over") 
+  else:
+    choice = input ("Where did you want to go, f for forest, m for mountain and s for sea")
+    if choice == 's' or choice =='S':
+      print("You found the treasure")
+    elif choice  == 'm' or choice == 'M':
+      print("You got attacked by a bear, Game over")
+    else:
+      print("You got lost in the forest, Game over")
+    
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
